92b777df2cea1850f5183f48c4e7c3256e4b33f4
j-adenilson <j.adenilson@outlook.com.br>
2025-07-15 20:28:01 -0300

    Trabalhando com tipos não primitivos

---
+++ Java10x/src/DadosNaoPrimitivo/DadosNaoPrimitivos.java
@@ -0,0 +1,19 @@
+package DadosNaoPrimitivo;
+
+public class DadosNaoPrimitivos {
+
+    public static void main(String[] args) {
+        //DADOS NÃO PRRIMITIVOS
+
+        //String, Array, Class, enum
+
+        String nome = "Naruto";
+        String sobreNome = "Uzumaki";
+        String aldeia = "Aldeia da folha";
+
+
+    }
+
+
+
+}
