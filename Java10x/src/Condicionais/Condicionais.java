ddc5c44f895fb619b1afb0d97a75e1d74cc31fce
j-adenilson <j.adenilson@outlook.com.br>
2025-07-15 20:43:49 -0300

    trabalhando com condicionais

--- Java10x/src/Condicionais/Condicionais.java
+++ Java10x/src/Condicionais/Condicionais.java
@@ -1,4 +1,19 @@
 package Condicionais;
 
 public class Condicionais {
+    public static void main(String[] args) {
+        String nome = "Naruto";
+        int idade = 20;
+        boolean hokage = false;
+        short numeroDeMissoes = 20;
+        String rank ;
+
+        if (numeroDeMissoes <= 10 && idade <= 15){
+            System.out.println("Rank: chunin");
+        } else if (numeroDeMissoes >= 20 && idade >= 20) {
+            System.out.println("Rank: Jounin");
+        } else{
+            System.out.println("Rank: Genin");
+        }
+    }
 }
