77e7740fb0973d4a8aeb85d9af82f04581bd62ec
j-adenilson <j.adenilson@outlook.com.br>
2025-07-21 22:41:28 -0300

    trabalhando com laços de repetição while e for

---
+++ Java10x/src/lacosDeRepeticao/LacosDeRepeticao.java
@@ -0,0 +1,19 @@
+package lacosDeRepeticao;
+
+public class LacosDeRepeticao {
+    public static void main(String[] args) {
+        int numeroDeClones = 0;
+        int numeroMaximoDeClones = 40;
+
+//        while (numeroDeClones <= numeroMaximoDeClones){
+//            numeroDeClones++;
+//            System.out.println("O naruto fez um clone das sombras " + numeroDeClones);
+//        }
+
+        for (int i = 0; i <= numeroMaximoDeClones; i++) {
+            System.out.println("O naruto fez um clone das sombras " + i);
+
+        }
+
+    }
+}
