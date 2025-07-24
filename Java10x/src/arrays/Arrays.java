e24f95de785c3825d339820cf2a4a507ffad2fea
j-adenilson <j.adenilson@outlook.com.br>
2025-07-23 22:46:14 -0300

    Trabalhando com Arrays

---
+++ Java10x/src/arrays/Arrays.java
@@ -0,0 +1,21 @@
+package arrays;
+
+public class Arrays {
+    public static void main(String[] args) {
+//        String ninja1 = "Naruto Uzumaki";
+//        String ninja2 = "Sasuke Uchiha";
+//        String ninja3 = "Sakura Haruno";
+//
+//        System.out.println(ninja1);
+//        System.out.println(ninja2);
+//        System.out.println(ninja3);
+
+
+        String[] ninja = new String[5];
+        ninja[0] = "Naruto Uzumaki";
+        ninja[1] = "Sasuke Uchiha";
+        ninja[2] = "Sakura Haruno";
+
+        System.out.println(ninja[0]);
+    }
+}
