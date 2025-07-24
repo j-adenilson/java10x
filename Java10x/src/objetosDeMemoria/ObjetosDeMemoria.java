703c0f194c2c7b959773847dbf3fde25c31c7a64
j-adenilson <j.adenilson@outlook.com.br>
2025-07-23 23:00:11 -0300

    iniciando tipos de arrays

---
+++ Java10x/src/objetosDeMemoria/ObjetosDeMemoria.java
@@ -0,0 +1,18 @@
+package objetosDeMemoria;
+
+public class ObjetosDeMemoria {
+    public static void main(String[] args) {
+        String[] ninja = new String[5];
+        ninja[0] = "Naruto Uzumaki";
+        ninja[1] = "Sasuke Uchiha";
+        ninja[2] = "Sakura Haruno";
+
+        System.out.println(ninja[0]);
+
+        int[] idade = new int[2];
+        idade[0] = 0;
+        idade[1] = 1;
+
+        System.out.println(idade[0]);
+    }
+}
