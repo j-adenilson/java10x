f1c38a5336aab14f116fa0e38a1485bbe4477387
j-adenilson <j.adenilson@outlook.com.br>
2025-08-13 23:17:04 -0300

    Construtores

--- Java10x/src/nivelIntermediario/Construtores/Hokage.java
+++ Java10x/src/nivelIntermediario/Construtores/Hokage.java
@@ -1,4 +1,18 @@
 package nivelIntermediario.Construtores;
 
 public class Hokage {
+    String nome;
+    int idade;
+    boolean vivoOuNao;
+
+    //construtor sem argumentos
+    public Hokage(){
+    }
+
+    //construtor com todos os argumentos
+    public Hokage(String nome, int idade, boolean vivoOuNao){
+        this.nome = nome;
+        this.idade = idade;
+        this.vivoOuNao = vivoOuNao;
+    }
 }
--- Java10x/src/nivelIntermediario/Construtores/Main.java
+++ Java10x/src/nivelIntermediario/Construtores/Main.java
@@ -1,4 +1,15 @@
 package nivelIntermediario.Construtores;
 
 public class Main {
+    public static void main(String[] args) {
+
+        Hokage Hashirama = new Hokage();
+        Hashirama.nome = "Hashirama Senju";
+        Hashirama.idade = 35;
+        Hashirama.vivoOuNao = false;
+
+        Hokage Tobirama = new Hokage("Tobirama Senju", 30, false);
+
+        Hokage Hiruzen = new Hokage("Hiruzen Sarutobi", 40, false);
+    }
 }
