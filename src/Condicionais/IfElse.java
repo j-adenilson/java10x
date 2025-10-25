04ec23c8f22ef10eb045d786971f6d4517a5fa6f
j-adenilson <j.adenilson@outlook.com.br>
2025-10-24 23:11:05 -0300

    Trabalhando com If e Else

--- src/Condicionais/IfElse.java
+++ src/Condicionais/IfElse.java
@@ -1,4 +1,25 @@
 package Condicionais;
 
-public class If {
+public class IfElse {
+    public static void main(String[] args) {
+        String nome = "Naruto Usumaki";
+        int idade = 16;
+        boolean hokage = false;
+        short numeroDeMissoes = 14;
+        String rank;
+
+        if(numeroDeMissoes <= 10){
+            System.out.println(nome + " Não está pronto para passar de nível");
+        } else if (numeroDeMissoes >= 11 || numeroDeMissoes <=30 ){
+            System.out.println(nome + " Está pronto para ser chunin");
+        } else if (numeroDeMissoes >= 31 || numeroDeMissoes <= 40) {
+            System.out.println(nome + " Está pronto para ser Jounin");
+        } else if (numeroDeMissoes > 41 ) {
+            System.out.println(nome + " Está pronto para ser da Anbu");
+        }else {
+            System.out.println("número de missoes invalidos");
+        }
+
+
+    }
 }
