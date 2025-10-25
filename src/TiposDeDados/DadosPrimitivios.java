1d77d93d34cb94184d7e54456ac3295090795297
j-adenilson <j.adenilson@outlook.com.br>
2025-10-24 22:18:17 -0300

    novo commit tipos de dados

---
+++ src/TiposDeDados/DadosPrimitivios.java
@@ -0,0 +1,29 @@
+package TiposDeDados;
+
+public class DadosPrimitivios {
+    static void main() {
+        String nome = "Naruto Usumaki";
+        int idade = 16;
+        double altura = 1.70;
+        char letra = 'N';
+        boolean vivoOuMorto = true;
+        Long saldoBancario = 9999999999999L;
+
+
+
+
+
+
+        /*
+                    Dados Primitivos Abordados
+            int: Números inteiros.
+            float: Números de ponto flutuante de precisão simples.
+            double: Números de ponto flutuante de precisão dupla.
+            char: Um único caractere.
+            boolean: Valores verdadeiros ou falsos.
+            byte: Números inteiros pequenos.
+            short: Números inteiros menores que um int.
+            Long: Números inteiros maiores que um int.
+         */
+    }
+}
