bd32f19878332e9cc63d0381539c58482e2e1b9e
j-adenilson <j.adenilson@outlook.com.br>
2025-10-26 17:33:52 -0300

    Trabalhando com Laços de repetição

---
+++ src/Condicionais/LacosDeRepeticao.java
@@ -0,0 +1,19 @@
+package Condicionais;
+
+public class LacosDeRepeticao {
+    public static void main(String[] args){
+
+        int numeroDeCLones = 0;
+        int numeroMaxDeClones = 40;
+
+        while (numeroDeCLones <= numeroMaxDeClones) {
+            System.out.println("O Naruto fez um clone " + numeroDeCLones);
+            numeroDeCLones ++;
+        }
+
+//        for (int i = 0; i <= numeroMaxDeClones; i++) {
+//            System.out.println("O Naruto fez um clone " + i);
+//        }
+
+    }
+}
