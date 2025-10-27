6769259e502d751d12705057ae5fd34138a9debe
j-adenilson <j.adenilson@outlook.com.br>
2025-10-27 11:56:27 -0300

    Adicionando novas funcionalidades

--- src/Desafios/Desafio02.java
+++ src/Desafios/Desafio02.java
@@ -14,28 +14,61 @@ public class Desafio02 {
 
 
 
-        while (opcoes != 3){
+        while (opcoes != 4){
 
             System.out.println("\n===== Menu Ninja =====");
             System.out.println("1. Cadastrar Ninja");
             System.out.println("2. Listar Ninjas");
-            System.out.println("3. Sair");
+            System.out.println("3. Deletar Ninja");
+            System.out.println("4. Sair");
             System.out.print("Escolha uma opção: ");
             opcoes = sc.nextInt();
             sc.nextLine();
 
             switch (opcoes){
+
+//                case 1:
+//                    if (ninjasCadastrados < numeroMax){
+//                        System.out.println("Digite o nome do ninja: ");
+//                        String nomeNinja = sc.nextLine();
+//
+//                        ninjas[ninjasCadastrados] = nomeNinja;
+//                        ninjasCadastrados ++;
+//                        System.out.println("Ninja cadastrado com sucesso!");
+//                    }else {
+//                        System.out.println("Lista está completa");
+//                    }
+//                    break;
+
+                    //===================================================
+
                 case 1:
-                    if (ninjasCadastrados < numeroMax){
-                        System.out.println("Digite o nome do ninja: ");
+                    if (ninjasCadastrados < numeroMax) {
+                        System.out.print("Digite o nome do ninja: ");
                         String nomeNinja = sc.nextLine();
-                        ninjas[ninjasCadastrados] = nomeNinja;
-                        ninjasCadastrados ++;
-                        System.out.println("Ninja cadastrado com sucesso!");
-                    }else {
-                        System.out.println("Lista está completa");
+
+                        //  Validação de nome duplicado
+                        boolean jaExiste = false;
+                        for (int i = 0; i < ninjasCadastrados; i++) {
+                            if (ninjas[i].equalsIgnoreCase(nomeNinja)) {
+                                jaExiste = true;
+                                break;
+                            }
+                        }
+
+                        if (jaExiste) {
+                            System.out.println(" Este ninja já foi cadastrado!");
+                        } else {
+                            ninjas[ninjasCadastrados] = nomeNinja;
+                            ninjasCadastrados++;
+                            System.out.println(" Ninja cadastrado com sucesso!");
+                        }
+
+                    } else {
+                        System.out.println(" Lista está completa!");
                     }
                     break;
+                //=================================================
                 case 2:
                     if (ninjasCadastrados == 0){
                         System.out.println("Nenhum ninja encontrado");
@@ -46,7 +79,41 @@ public class Desafio02 {
                         }
                     }
                     break;
+
+                    //==========================================================
+
                 case 3:
+                    if (ninjasCadastrados == 0) {
+                        System.out.println("Nenhum ninja para deletar.");
+                        break;
+                    }
+
+                    System.out.print("Digite o nome do ninja que deseja deletar: ");
+                    String deletar = sc.nextLine();
+                    boolean encontrado = false;
+
+                    // procura o ninja pelo nome
+                    for (int i = 0; i < ninjasCadastrados; i++) {
+                        if (ninjas[i].equalsIgnoreCase(deletar)) {
+                            // move todos os próximos elementos uma posição para trás
+                            for (int j = i; j < ninjasCadastrados - 1; j++) {
+                                ninjas[j] = ninjas[j + 1];
+                            }
+                            ninjasCadastrados--; // diminui o total
+                            ninjas[ninjasCadastrados] = null; // limpa o último espaço
+                            encontrado = true;
+                            System.out.println("Ninja \"" + deletar + "\" deletado com sucesso!");
+                            break;
+                        }
+                    }
+
+                    if (!encontrado) {
+                        System.out.println("Ninja não encontrado!");
+                    }
+                    break;
+
+                    //===========================================================
+                case 4:
                     System.out.println("Encerrando o programa...");
                     break;
                 default:
