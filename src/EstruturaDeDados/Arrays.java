10481d411e28da8891f739e37bb28c09d36737cc
j-adenilson <j.adenilson@outlook.com.br>
2025-10-27 10:14:01 -0300

    Trabalhando com Array

---
+++ src/EstruturaDeDados/Arrays.java
@@ -0,0 +1,14 @@
+package EstruturaDeDados;
+
+public class Arrays {
+    public static void main(String[] args){
+
+       String[] ninjas = new String[5];
+       ninjas[0] = "Naruto Uzumaki";
+       ninjas[1] = "Sasuke Uchiha";
+       ninjas[3] = "Sakura Haruno";
+
+
+
+    }
+}
