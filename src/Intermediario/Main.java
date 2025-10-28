912d50020785abe72247986d8798a10829d51433
j-adenilson <j.adenilson@outlook.com.br>
2025-10-28 10:44:34 -0300

    Metodos e parametros

--- src/Intermediario/Main.java
+++ src/Intermediario/Main.java
@@ -6,19 +6,23 @@ public class Main {
         //Ninja 1
         String nome = "Naruto Uzumaki";
         String aldeia = "ALdeia da folha";
-        int idade = 16;
+        int idade = 30;
 
         //Ninja 2
         Ninja Naruto = new Ninja();
         Naruto.nome = "Naruto Uzumaki";
         Naruto.aldeia = "Aldeia da folha";
-        Naruto.idade = 16;
-        System.out.println(Naruto.nome);
+        Naruto.idade = 30;
+        String boasVindas = Naruto.boasVindasMeuNomeE();
+        System.out.println(boasVindas);
+        int qtFalta = Naruto.anosParaSerHokage(60);
+        System.out.println("faltam " + qtFalta + " anos");
 
         //Ninja 3
         Ninja Sasuke = new Ninja();
         Sasuke.nome = "Sasuke Uchiha";
         Sasuke.aldeia = "Aldeia da folha";
         Sasuke.idade = 18;
+        Sasuke.SharinganAtivado();
     }
 }
--- src/Intermediario/Ninja.java
+++ src/Intermediario/Ninja.java
@@ -4,4 +4,16 @@ public class Ninja {
     String nome;
     String aldeia;
     int idade;
+
+    public void SharinganAtivado(){
+        System.out.println("O charingan ativou");
+    }
+
+    public String boasVindasMeuNomeE(){
+        return "Olá, meu nome é " + nome;
+    }
+
+    public int anosParaSerHokage(int idadeMinima){
+        return idadeMinima - idade;
+    }
 }
