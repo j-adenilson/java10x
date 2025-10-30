6c2b1510e0ac38d67dc8ea342d32a97c31e3b668
j-adenilson <j.adenilson@outlook.com.br>
2025-10-30 20:21:58 -0300

    List - Um array com super poderes

--- src/Intermediario/Main.java
+++ src/Intermediario/Main.java
@@ -1,13 +1,27 @@
 package Intermediario;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Main {
     public static void main (String[] args){
 
-        System.out.println("==========Naruto Uzuaki=========");
+        //Listas
+        List<String> ninjasList = new ArrayList<>();
+        ninjasList.add("Naruto Uzuaki");
+        ninjasList.add("Sasuke Uchiha");
+        ninjasList.add("Sakura Haruno");
+        ninjasList.add("Kakashi Hatake");
+        System.out.println(ninjasList);
 
+        ninjasList.remove("Kakashi Hatake");
+        System.out.println(ninjasList);
 
+        ninjasList.set(2, "Hashirama Senju");
+        System.out.println(ninjasList);
 
-        System.out.println("==========Sasuke Uchiha=========");
+        System.out.println("Tamanho da lista: " + ninjasList.size() + " Elementos");
+
 
     }
 }
