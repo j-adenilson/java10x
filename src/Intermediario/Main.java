cd6547a6bff29e735c10f903b2112e3ed9e063a9
j-adenilson <j.adenilson@outlook.com.br>
2025-10-31 13:01:41 -0300

    Stack - O ultimo a entrar é o primeiro a sair

--- src/Intermediario/Main.java
+++ src/Intermediario/Main.java
@@ -2,12 +2,30 @@ package Intermediario;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Stack;
 
 public class Main {
     public static void main (String[] args) {
 
-        NinjaRecord cadastro = new NinjaRecord("Naruto", "naruto@email", 9999999);
+    //Array
+    //São estaticos e tem referencia de memmoria
+        String[] ninjasArray = new String[2];
+        ninjasArray[0] = "Naruto";  //adicionando item
 
-        System.out.println(cadastro.);
+    //Listas
+    // São dinamicas e tamanho aumenta e diminui conforme precisa
+        List<String> ninjasList = new ArrayList<>();
+        ninjasList.add("naruto"); //adicionando item
+
+    //Stack
+    //O ultimmo elemento que entrou vai ser obrigatoriamente o primeiro a sair
+        Stack<String> ninjasStack = new Stack<>();
+        ninjasStack.add("Naruto");  //adicionando item
+        ninjasStack.push("Sasuke");
+        ninjasStack.push("Sakura");
+        System.out.println(ninjasStack);
+        ninjasStack.pop();  //removendo item
+        System.out.println(ninjasStack);
+
     }
 }
