399bf98f2d77f83b0c413e89ffc37db579f870e7
j-adenilson <j.adenilson@outlook.com.br>
2025-11-01 21:01:29 -0300

    Queue

--- src/Intermediario/Main.java
+++ src/Intermediario/Main.java
@@ -1,8 +1,6 @@
 package Intermediario;
 
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Stack;
+import java.util.*;
 
 public class Main {
     public static void main (String[] args) {
@@ -27,5 +25,25 @@ public class Main {
         ninjasStack.pop();  //removendo item
         System.out.println(ninjasStack);
 
+        //Queue // Filas
+        Queue<String> ninjasQueue = new LinkedList<>();
+        ninjasQueue.add("Naruto");
+        ninjasQueue.add("Sasuke");
+        ninjasQueue.add("Sakura");
+        ninjasQueue.add("Tobirama");
+        ninjasQueue.add("Hashirama");
+
+        System.out.println("Ninjas na fila " + ninjasQueue);
+        ninjasQueue.poll(); //removendo o primeiro item(Head) da fila
+        System.out.println("primeiro da fila: " + ninjasQueue.peek()); // ve quem é o primeiro da fila
+        ninjasQueue.add("Tsunade"); // adicionando item na lista
+        System.out.println(ninjasQueue);
+        // nao é posivel deletar o ultimo
+        if (ninjasQueue.isEmpty()){
+            System.out.println("a fila esta vazia"); // verificando se a fila esta vazia
+        }
+
+
     }
+
 }
