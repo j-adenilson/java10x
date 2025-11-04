9fe732784ca92689fde8b6e06adf2c7deebd091b
j-adenilson <j.adenilson@outlook.com.br>
2025-11-03 22:48:36 -0300

    LinkedList - introdução a Collection

--- src/Intermediario/Main.java
+++ src/Intermediario/Main.java
@@ -5,43 +5,22 @@ import java.util.*;
 public class Main {
     public static void main (String[] args) {
 
-    //Array
-    //São estaticos e tem referencia de memmoria
-        String[] ninjasArray = new String[2];
-        ninjasArray[0] = "Naruto";  //adicionando item
+    String[] aray = new String[5];
+    ArrayList<String> arrayList =new ArrayList<>();
+    Stack<String> stack = new Stack<>();
+    Queue<String> queue = new LinkedList<>();
+    PriorityQueue<String> priorityQueue = new PriorityQueue<>();
 
-    //Listas
-    // São dinamicas e tamanho aumenta e diminui conforme precisa
-        List<String> ninjasList = new ArrayList<>();
-        ninjasList.add("naruto"); //adicionando item
+    LinkedList<String> linkedList = new LinkedList<>();
+    linkedList.add("Naruto");
+    linkedList.add("Sasuke");
+    linkedList.add("Sakura");
 
-    //Stack
-    //O ultimmo elemento que entrou vai ser obrigatoriamente o primeiro a sair
-        Stack<String> ninjasStack = new Stack<>();
-        ninjasStack.add("Naruto");  //adicionando item
-        ninjasStack.push("Sasuke");
-        ninjasStack.push("Sakura");
-        System.out.println(ninjasStack);
-        ninjasStack.pop();  //removendo item
-        System.out.println(ninjasStack);
+    System.out.println(linkedList);
+    linkedList.add(1, "kakashi");
+    System.out.println(linkedList);
 
-        //Queue // Filas
-        Queue<String> ninjasQueue = new LinkedList<>();
-        ninjasQueue.add("Naruto");
-        ninjasQueue.add("Sasuke");
-        ninjasQueue.add("Sakura");
-        ninjasQueue.add("Tobirama");
-        ninjasQueue.add("Hashirama");
 
-        System.out.println("Ninjas na fila " + ninjasQueue);
-        ninjasQueue.poll(); //removendo o primeiro item(Head) da fila
-        System.out.println("primeiro da fila: " + ninjasQueue.peek()); // ve quem é o primeiro da fila
-        ninjasQueue.add("Tsunade"); // adicionando item na lista
-        System.out.println(ninjasQueue);
-        // nao é posivel deletar o ultimo
-        if (ninjasQueue.isEmpty()){
-            System.out.println("a fila esta vazia"); // verificando se a fila esta vazia
-        }
 
 
     }
