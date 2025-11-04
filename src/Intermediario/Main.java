0464831debd8f5da35215c50defbbc89ae7eff5b
j-adenilson <j.adenilson@outlook.com.br>
2025-11-03 23:13:08 -0300

    LinkedHashSet

--- src/Intermediario/Main.java
+++ src/Intermediario/Main.java
@@ -12,11 +12,25 @@ public class Main {
     ninjas.add("Naruto");
     System.out.println("ninjas " + ninjas);
 
-    Set<String> ninjasSet = new HashSet<>();
+    Set<String> ninjasSet = new HashSet<>(); //Utilizado quando nao quero que apareça item duplicado
     ninjasSet.addAll(ninjas);
     System.out.println("NinjasSet: " + ninjasSet);
     ninjasSet.remove("Sasuke");
-        System.out.println(ninjasSet);
+    System.out.println(ninjasSet);
+
+    Set<String> ninjasTreeSet = new TreeSet<>(); // Utilizado quando nao quero que apareça ite duplicado e quero que apareçam em ordem alfabetica
+    ninjasTreeSet.add("Minato");
+    ninjasTreeSet.add("Hasirama");
+    ninjasTreeSet.add("Tobirama");
+    ninjasTreeSet.add("Aokiji");
+    System.out.println("ninjas TreeSet: " + ninjasTreeSet);
+
+
+        Set<String> ninjasHashSet = new LinkedHashSet<>();//imprime na ordem de implementação
+        ninjasHashSet.add("Naruto");
+        ninjasHashSet.add("Sasuke");
+        ninjasHashSet.add("Sakura");
+        System.out.println("ninjas linkedHashSet:" + ninjasHashSet);
 
 
 
